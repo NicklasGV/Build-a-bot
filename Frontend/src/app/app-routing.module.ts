a7441ff8573f61382ffa780a4b3a3cc77544b007
Nicklas Vedeby <nvedeby@gmail.com>
2025-04-07 13:21:15 +0200

    Fonts added

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadComponent: () => 
+    import('./components/home/home.component').then(it => it.HomeComponent)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
