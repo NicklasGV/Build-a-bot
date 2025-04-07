53d6ecfd41c366ee38149d0959bf8b7da2b48822
Nicklas Vedeby <nvedeby@gmail.com>
2025-04-07 19:42:02 +0200

    Added header template to frontend

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { FooterComponent } from './shared/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FooterComponent
+    FooterComponent,
+    HeaderComponent
   ],
   providers: [
     provideClientHydration()
