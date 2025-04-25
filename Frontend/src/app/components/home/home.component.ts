d427b87d8df8f70568f4e2fa4f68b0b8bea11ddc
Nicklas Vedeby <nvedeby@gmail.com>
2025-04-25 10:49:36 +0200

    Added dashboards to home hero buttons

--- Frontend/src/app/components/home/home.component.ts
+++ Frontend/src/app/components/home/home.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { User, resetUser } from '../../models/user.model';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { RoleId } from '../../models/role.model';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  currentUser: User = resetUser();
+  isLoggedIn: boolean = false;
+  public RoleId = RoleId;
 
+  constructor(
+      private authService: AuthService
+    ) {
+      this.authService.currentUser.subscribe((x) => (this.currentUser = x));
+  }
+
+  ngOnInit(): void {
+    this.authService.currentUser.subscribe(user => {
+      this.currentUser = user;
+      this.isLoggedIn = !!user.token;
+    });
+  }
 }
