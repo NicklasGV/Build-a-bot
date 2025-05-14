fa4c03c24499dc87c45adaf5fb5eee17de87309c
Nicklas Vedeby <nvedeby@gmail.com>
2025-05-14 08:46:17 +0200

    Update environment.ts

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
     devApiUrl: 'https://buildabot.dk/',
     discord: {
         clientId:    '1364129896376569877',
-        redirectUri: 'http://localhost:4200/auth/discord/callback',
+        redirectUri: 'https://buildabot.dk/auth/discord/callback',
         scopes:      ['identify', 'email'],
         authorizeUrl:'https://discord.com/api/oauth2/authorize',
         tokenUrl:    'https://discord.com/api/oauth2/token',
